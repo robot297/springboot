3424b8da3aa86a7947bd9c1ff7160495884fa72a
Daniel Obot <danobot@hotmail.com>
2019-10-16 20:00:25 -0500

    Code examples.

---
+++ java/src/main/java/Application.java
@@ -0,0 +1,12 @@
+import Connections.*;
+
+public class Application {
+    public static void main(String[] args) {
+
+        Accounts accounts = new Accounts();
+        Rates rates = new Rates();
+
+        System.out.println(accounts.getUsers());
+        System.out.println(rates.getAutoLoanRates());
+    }
+}
