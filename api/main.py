9dc217c5d2148068371941dbd6341f9930aa894f
Daniel Obot <danobot@hotmail.com>
2020-11-05 20:19:40 -0600

    feat: add create_db end point and app route.

--- api/main.py
+++ api/main.py
@@ -1,5 +1,19 @@
 """Basic API functions"""
 from flask import Response
+from config import my_app
+from db_config import db
+from models.user_model import User
+
+
+@my_app.route('/dbsetup')
+def create_db():
+    """Creates database with some data populated."""
+    db.drop_all()
+    db.create_all()
+    User.add_user_td()
+    response_text = '{ "message": "Database created" }'
+    response = Response(response_text, 200, mimetype='application/json')
+    return response
 
 
 def welcome():
