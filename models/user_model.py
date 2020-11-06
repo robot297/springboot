f6a21d127e457c03f1fe755e0d4ac714e098e4b6
Daniel Obot <danobot@hotmail.com>
2020-11-05 20:14:31 -0600

    feat: added user model with basic function

---
+++ models/user_model.py
@@ -0,0 +1,23 @@
+"""Model for users and database methods"""
+from db_config import db
+
+
+class User(db.Model):
+    """Model for users"""
+    __tablename__ = 'users'
+    username = db.Column(db.String(80), primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    @staticmethod
+    def add_user(_username, _email):
+        """Function adds a user to a db table."""
+        new_user = User(username=_username, email=_email)
+        db.session.add(new_user)
+        db.session.commit()
+
+    @staticmethod
+    def add_user_td():
+        """Adds test users to the database"""
+        User.add_user("darth", "darth.vader@gmail.com")
+        User.add_user("superman", "super.man@gmail.com")
+        User.add_user("thor", "thor@gmail.com")
