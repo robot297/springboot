f1737d865c198c8444a4346904d1ba36ea179300
Daniel Obot <danobot@hotmail.com>
2020-11-12 10:15:14 -0600

    chore: add get_by_username to api users.py file

--- api/users.py
+++ api/users.py
@@ -1,5 +1,5 @@
 """Controller to route user functionality"""
-from flask import jsonify
+from flask import jsonify, Response
 from models.user_model import User
 
 
@@ -7,3 +7,9 @@ def get_users():
     """Gets all users"""
     return_value = jsonify({'users': User.get_all_users()})
     return return_value
+
+
+def get_by_username(username):
+    """Gets a user by username"""
+    response = Response(str(User.get_user(username)), 200, mimetype="application/json")
+    return response
